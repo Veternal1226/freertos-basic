f2ae30bc960aac9da8ed81b54d08ac8e6fac38da
Sirius Wang <larrywhy.tw@gmail.com>
2015-04-23 00:55:01 +0800

    ps_command:Remove unsigned declaration

--- src/shell.c
+++ src/shell.c
@@ -98,7 +98,8 @@ int filedump(const char *filename){
 }
 
 void ps_command(int n, char *argv[]){
-	signed char buf[1024];
+	char buf[1024];
+	//char * buf;
 	vTaskList(buf);
         fio_printf(1, "\n\rName          State   Priority  Stack  Num\n\r");
         fio_printf(1, "*******************************************\n\r");
